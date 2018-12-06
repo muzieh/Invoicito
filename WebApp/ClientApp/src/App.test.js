b3c103d9719f8a7db09296e3e453668bc07b816f
Marcin Owerczuk <muzieh@gmail.com>
2018-12-06 00:46:19 +0000

    add invoice module

--- WebApp/ClientApp/src/App.test.js
+++ WebApp/ClientApp/src/App.test.js
@@ -6,3 +6,8 @@ it('renders without crashing', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(<App />, div);
 });
+
+
+test('test on test', () => {
+	expect(2 + 2).toBe(4);
+});
--- WebApp/ClientApp/src/components/AddInvoice.js
+++ WebApp/ClientApp/src/components/AddInvoice.js
@@ -1,12 +1,26 @@
-﻿import React from 'react';
+﻿import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-const AddInvoice = props => (
+//export default connect()(addinvoice);
 
-	<div>
-		<h1>Add invoice</h1>
-		<input type="text"  />
-	</div>
-);
+class AddInvoice extends React.Component {
+	render() {
+		return (
+			<div className="col-9">
+				<form className="form-signin">
+					<h2 className="form-signin-heading">Add invoice</h2>
+					<label htmlFor="invoice-number" className="sr-only">invoice number</label>
+					<input type="text" id="invoice-number" className="form-control" placeholder="invoice number" required autoFocus />
+					<label htmlFor="inputpassword" className="sr-only">password</label>
+					<input type="text" id="invoice-number" className="form-control" placeholder="invoice number" required />
+						<div className="checkbox">
+							<label><input type="checkbox" value="remember-me" /> remember me</label>
+						</div>
+					<button className="btn btn-lg btn-primary btn-block" type="submit">Save invoice</button>
+				</form>
+			</div>
+		);
+	}
+}
 
-export default connect()(AddInvoice);
+export default AddInvoice;
